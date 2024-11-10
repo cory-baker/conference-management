f2b3ba92614397c3447a15fff3952126d59c3395
Cory Baker <coryb08@gmail.com>
2024-11-10 13:46:16 -0600

    dev env setup

--- app.rb
+++ app.rb
@@ -1,6 +1,24 @@
 require 'sinatra'
+require 'mysql2'
+
+client = Mysql2::Client.new(
+  host: 'localhost',
+  username: 'root',
+  password: 'root',
+  database: 'conference'
+)
+
+
 set :bind, '0.0.0.0'
 
 get '/' do
-  "Hello, Sinatra on Docker!"
+  erb :index
+end
+
+post '/register' do
+  name = params[:name]
+  email = params[:email]
+  phone = params[:phone]
+  client.query("INSERT INTO attendees (name, email, phone) VALUES ('#{name}', '#{email}', '#{phone}')")
+  erb :thanks
 end
